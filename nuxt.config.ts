42d95dd976b0cdae99932e855ad67eadc5daf001
Richard Tran <richard.tran@razorfish.fr>
2026-01-11 18:07:55 +0100

    Init tailwind + init standard files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Nuxt Catalogue international', // default fallback title
+      htmlAttrs: {
+        lang: 'fr',
+      },
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    },
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   typescript: {
     typeCheck: true,
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
   },
 })
