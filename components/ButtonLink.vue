a8a053a3d5868103ff0d11c5c253ce46c692f55c
Richard Tran <richard.tran@razorfish.fr>
2026-01-11 22:26:13 +0100

    Create component Button

---
+++ components/ButtonLink.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtLink to="/countries" class="btn-primary">
+    <slot />
+  </NuxtLink>
+</template>
+
+<style scoped>
+.btn-primary {
+  @apply bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700;
+}
+</style>
