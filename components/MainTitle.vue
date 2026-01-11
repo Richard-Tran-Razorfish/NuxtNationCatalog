1aa22c6209546ef769b8634f6c9ca446359772c9
Richard Tran <richard.tran@razorfish.fr>
2026-01-11 22:26:27 +0100

    Create component Maintitle

---
+++ components/MainTitle.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="mainTitle__title">
+      <slot />
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+.mainTitle__title {
+  @apply
+  text-3xl
+  font-bold
+  underline
+  text-center;
+}
+</style>
