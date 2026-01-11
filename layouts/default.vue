ca35db2d168adcc3aa46391b628fbe5d7affc9d0
Richard Tran <richard.tran@razorfish.fr>
2026-01-11 22:26:46 +0100

    Add style for layout default

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
+  <div class="layoutsDefault__container">
     <slot />
   </div>
 </template>
+
+<style scoped>
+.layoutsDefault__container {
+  @apply
+  mx-4
+  lg:mx-16
+}
+</style>
